c5f1eb3fd850fc793cd0208cc449f3be5718f052
Anthony Sottile <asottile@umich.edu>
2017-03-23 09:46:33 -0700

    v1.0.6

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Sample `.pre-commit-config.yaml`:
 
 ```yaml
 -   repo: https://github.com/asottile/pyupgrade
-    sha: v1.0.5
+    sha: v1.0.6
     hooks:
     -   id: pyupgrade
 ```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pyupgrade',
     description='A tool to automatically upgrade syntax for newer versions.',
     url='https://github.com/asottile/pyupgrade',
-    version='1.0.5',
+    version='1.0.6',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
