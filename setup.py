3a9f62ff899fbd5ddbb9202fdd222244ffa6f3b5
Anthony Sottile <asottile@umich.edu>
2019-02-27 22:10:55 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,24 +1,2 @@
 from setuptools import setup
-
-setup(
-    name='pyupgrade',
-    description='A tool to automatically upgrade syntax for newer versions.',
-    url='https://github.com/asottile/pyupgrade',
-    version='1.11.3',
-    author='Anthony Sottile',
-    author_email='asottile@umich.edu',
-    license='MIT',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    install_requires=['tokenize-rt>=2.1'],
-    py_modules=['pyupgrade'],
-    entry_points={'console_scripts': ['pyupgrade = pyupgrade:main']},
-)
+setup()
