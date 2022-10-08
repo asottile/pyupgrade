07ec3e90cb646420fb781f46455c905bb7eb4a2b
Lev <levblt1@gmail.com>
2022-10-08 15:21:23 +0300

    rename class names to be different in each test case

--- tests/features/type_bases_object_test.py
+++ tests/features/type_bases_object_test.py
@@ -10,7 +10,7 @@ from pyupgrade._main import _fix_plugins
     ('src', 'expected'),
     (
         ('A = type("A", (), {})', 'A = type("A", (), {})'),
-        ('type("C", (int,), {}', 'type("C", (int,), {}'),
+        ('B = type("B", (int,), {}', 'B = type("B", (int,), {}'),
     ),
 )
 def test_fix_type_bases_object_noop(src, expected):
@@ -21,11 +21,11 @@ def test_fix_type_bases_object_noop(src, expected):
 @pytest.mark.parametrize(
     ('s', 'expected'),
     (
-        ('type("A", (object,), {})', 'type("A", (), {})'),
-        ('type("B", (object, tuple), {})', 'type("B", (tuple,), {})'),
-        ('type("B", (object, foo, bar), {})', 'type("B", (foo, bar), {})'),
-        ('type("B", (tuple, object), {})', 'type("B", (tuple,), {})'),
-        ('type("B", (foo, bar, object), {})', 'type("B", (foo, bar), {})'),
+        ('A = type("A", (object,), {})', 'A = type("A", (), {})'),
+        ('B = type("B", (object, tuple), {})', 'B = type("B", (tuple,), {})'),
+        ('C = type("C", (object, foo, bar), {})', 'C = type("C", (foo, bar), {})'),
+        ('D = type("D", (tuple, object), {})', 'D = type("D", (tuple,), {})'),
+        ('E = type("E", (foo, bar, object), {})', 'E = type("E", (foo, bar), {})'),
     ),
 )
 def test_fix_type_bases_object(s, expected):
