17c16aa7abd444332e6706274a27203eb7423611
Lev <levblt1@gmail.com>
2022-10-11 18:18:51 +0300

    rewrite replacemnt function to use parse_call_args instead of going through all tokens

--- pyupgrade/_plugins/type_bases_object.py
+++ pyupgrade/_plugins/type_bases_object.py
@@ -5,82 +5,53 @@ from typing import Iterable
 
 from tokenize_rt import Offset
 from tokenize_rt import Token
+from tokenize_rt import UNIMPORTANT_WS
 
 from pyupgrade._ast_helpers import ast_to_offset
 from pyupgrade._data import register
 from pyupgrade._data import State
 from pyupgrade._data import TokenFunc
-from pyupgrade._token_helpers import find_closing_bracket
 from pyupgrade._token_helpers import find_open_paren
+from pyupgrade._token_helpers import parse_call_args
 
 
 def remove_base_class_from_type_call(i: int, tokens: list[Token]) -> None:
-    # look forward and backward to find commas / parens
-    brace_stack = []
-    j = i
-    while tokens[j].src != ',':
-        if tokens[j].src == ')':
-            brace_stack.append(j)
-        j += 1
-    right = j
-
-    # if there's a close-paren after a trailing comma
-    j = right + 1
-    if not brace_stack and tokens[j].src != ')':
-        while tokens[j].src != ')' and tokens[j].name != 'NAME':
-            j += 1
-        right = j
-
-    if brace_stack:
-        last_part = brace_stack[-1]
-    else:  # for trailing commas
-        last_part = i if tokens[i].src == ',' else i + 1
-
-    j = i
-
-    while tokens[j].src not in {',', '('}:
-        j -= 1
-    left = j
-
-    # single base, remove the entire bases
-    if tokens[left].src == '(' and tokens[right].src == ',':
-        del tokens[left + 1:right + 1]
-    # multiple bases, base is first
-    elif tokens[left].src == '(' and tokens[right].src != ')':
-        # only one base will be left, (tuple) -> (tuple,)
-        if sum(
-            1 if token.name == 'NAME' else 0
-            for token in tokens[left:find_closing_bracket(tokens, left)]
-        ) == 2:
-            tokens.insert(right + 1, Token('OP', ','))
-        # we should preserve the newline
-        if tokens[left + 1].name == 'NL':
-            # we should also preserve indents
-            if tokens[left + 2].name == 'UNIMPORTANT_WS':
-                del tokens[left + 3:right]
-            else:
-                del tokens[left + 2:right]
-        else:
-            del tokens[left + 1:right]
-    # multiple bases, base is not first
-    else:
-        type_call_open = find_open_paren(tokens, 0)
-        bases_open = find_open_paren(tokens, type_call_open + 1)
-        # only one base will be left, (tuple) -> (tuple,)
-        if sum(
-            1 if token.name == 'NAME' else 0
-            for token in tokens[bases_open:last_part + 1]
-        ) == 2:
-            del tokens[left + 1:last_part]
-        # we should preserve indents
-        elif tokens[last_part - 1].name == 'UNIMPORTANT_WS':
-            # we should also preserve the newline
-            if tokens[last_part - 2].name == 'NL':
-                del tokens[left:last_part - 2]
-            else:
-                del tokens[left:last_part - 1]
-        else:
-            del tokens[left:last_part]
+    type_start = find_open_paren(tokens, 0)
+    bases_start = find_open_paren(tokens, type_start + 1)
+    bases, end = parse_call_args(tokens, bases_start)
+    for base_start, base_end in bases:
+        for token in tokens[base_start:base_end]:
+            if token.src == 'object':
+                object_index = bases.index((base_start, base_end))
+                # handle object first
+                if object_index == 0:
+                    # (object, tuple[,]) -> (tuple,)
+                    if len(bases) == 2:
+                        tokens.insert(bases[1][1], Token('OP', ','))
+                    # (object, foo, bar, ...[,]) -> (foo, bar, ...[,])
+                    if len(bases) >= 2:
+                        next_arg_start = bases[1][0]
+                        # preserve newlines
+                        if tokens[next_arg_start].name == 'NL':
+                            del tokens[base_start:next_arg_start]
+                        else:
+                            del tokens[base_start:bases[1][0] + 1]
+                    # (object,) -> ()
+                    else:
+                        del tokens[base_start:base_end + 1]
+                # handle object last
+                elif object_index == len(bases) - 1:
+                    # (tuple, object[,]) -> (tuple,)
+                    if len(bases) == 2:
+                        del tokens[base_start:base_end]
+                    # (foo, bar, ..., object[,]) -> (foo, bar, ...[,])
+                    else:
+                        if tokens[base_end - 1].name == UNIMPORTANT_WS and \
+                                tokens[base_end - 2].name == 'NL':
+                            del tokens[base_start - 1:base_end - 2]
+                        else:
+                            del tokens[base_start - 1:base_end]
+                return
 
 
 @register(ast.Call)
