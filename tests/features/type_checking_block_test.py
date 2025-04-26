ecd46f77ebad448e52f595ab81367036a034278f
Adam <1306402+AtomsForPeace@users.noreply.github.com>
2025-04-26 21:07:59 +0200

    update test name

--- tests/features/type_checking_block_test.py
+++ tests/features/type_checking_block_test.py
@@ -21,6 +21,6 @@ from pyupgrade._main import _fix_plugins
         ),
     ),
 )
-def test_fix_typing_text(s, expected):
+def test_remove_type_checking_block_for_protocol(s, expected):
     ret = _fix_plugins(s, settings=Settings(min_version=(3, 8)))
     assert ret == expected
