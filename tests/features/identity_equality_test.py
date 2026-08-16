2b36a3300bdceda4ed3f490d1c357b80f9ef5102
Andrej730 <azhilenkov@gmail.com>
2026-08-16 18:55:51 +0500

    identity_equality: add test for tuples

--- tests/features/identity_equality_test.py
+++ tests/features/identity_equality_test.py
@@ -15,12 +15,14 @@ from pyupgrade._main import _fix_plugins
         'x is ...',
         'x is (not 5)',
         'x is 5 + 5',
-        # pyupgrade is timid about containers since the original can be
+        # pyupgrade is timid about mutable containers since the original can be
         # always-False, but the rewritten code could be `True`.
-        'x is ()',
         'x is []',
         'x is {}',
         'x is {1}',
+        # Tuple with a non-constants is also always-False.
+        'x is ([1,2,3],)',
+        'x is ((1,2,3),)',
     ),
 )
 def test_fix_is_compare_to_literal_noop(s):
@@ -38,6 +40,10 @@ def test_fix_is_compare_to_literal_noop(s):
         pytest.param('x is b""', 'x == b""', id='bytes'),
         pytest.param('x is 1.5', 'x == 1.5', id='float'),
         pytest.param('x is 2j', 'x == 2j', id='complex'),
+        pytest.param('x is (1,2,3)', 'x == (1,2,3)', id='tuple'),
+        pytest.param('x is (None,)', 'x == (None,)', id='tuple with None'),
+        pytest.param('x is (True, 1)', 'x == (True, 1)', id='tuple with bool'),
+        pytest.param('x is (...,)', 'x == (...,)', id='tuple with ellipsis'),
         # Regression tests - ensure we don't mistake those cases for `x is True`.
         pytest.param('x is 1', 'x == 1', id='int equals bool True'),
         pytest.param('x is 1.0', 'x == 1.0', id='float equals bool True'),
