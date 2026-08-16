9ec614175a60e2473e5eb7d034d8b688676f42ae
Andrej730 <azhilenkov@gmail.com>
2026-08-16 18:37:32 +0500

    identity_equality: add test for `complex`

--- tests/features/identity_equality_test.py
+++ tests/features/identity_equality_test.py
@@ -35,6 +35,7 @@ def test_fix_is_compare_to_literal_noop(s):
         pytest.param('x is u""', 'x == u""', id='unicode string'),
         pytest.param('x is b""', 'x == b""', id='bytes'),
         pytest.param('x is 1.5', 'x == 1.5', id='float'),
+        pytest.param('x is 2j', 'x == 2j', id='complex'),
         # Regression tests - ensure we don't mistake those cases for `x is True`.
         pytest.param('x is 1', 'x == 1', id='int equals bool True'),
         pytest.param('x is 1.0', 'x == 1.0', id='float equals bool True'),
